3f285ebc51060bc7eb02237ea52c969e898f6db4
jy2947git <jy2947@yahoo.com>
2010-07-02 16:50:04 -0500

    w

--- test/unit/server_test.rb
+++ test/unit/server_test.rb
@@ -5,4 +5,25 @@ class ServerTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "can describe ec2 instance" do
+    ec2 = Server.new()
+    res = ec2.describe_instance("i-9894aaf0")
+    puts res["reservationSet"]["item"][0]["reservationId"]
+    puts res["reservationSet"]["item"][0]["instancesSet"]["item"][0]["imageId"]
+    puts res["reservationSet"]["item"][0]["instancesSet"]["item"][0]["instanceType"]
+    puts res["reservationSet"]["item"][0]["instancesSet"]["item"][0]["instanceState"]["name"]
+  end
+
+  test "can start ec2 instance" do
+    ec2 = Server.new()
+    res = ec2.start_instance("i-9894aaf0")
+    puts res
+  end
+
+  test "can stop ec2 instance" do
+    ec2 = Server.new()
+    res = ec2.stop_instance("i-9894aaf0")
+    puts res
+  end
 end
